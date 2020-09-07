bc55eb20b138673ba6a733d502b70231ebfdf93a
Sophie B. <stb.github@gmail.com>
2020-09-07 10:56:35 +0200

    Add reminders works

--- app/src/main/java/com/example/madlevel2example/ReminderAdapter.kt
+++ app/src/main/java/com/example/madlevel2example/ReminderAdapter.kt
@@ -7,17 +7,25 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel2example.databinding.ItemReminderBinding
 
 class ReminderAdapter(private val reminders: List<Reminder>) : RecyclerView.Adapter<ReminderAdapter.ViewHolder>() {
-
+    /**
+     * Creates and returns a ViewHolder object, inflating a standard layout called simple_list_item_1.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.item_reminder, parent, false)
         )
     }
 
+    /**
+     * Returns the size of the list
+     */
     override fun getItemCount(): Int {
         return reminders.size
     }
 
+    /**
+     * Called by RecyclerView to display the data at the specified position.
+     */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.databind(reminders[position])
     }
