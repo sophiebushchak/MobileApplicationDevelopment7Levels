60b5ec4abfc22c1c337eedbce8949c22fa76537d
Sophie B. <stb.github@gmail.com>
2020-09-08 11:55:32 +0200

    Item touch helper semi-functionality

--- app/src/main/java/com/example/madlevel2task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task2/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.madlevel2task2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel2task2.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val questions = arrayListOf<Question>()
@@ -20,14 +22,64 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
-        binding.rvQuestions.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        binding.rvQuestions.layoutManager =
+            LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
         binding.rvQuestions.adapter = questionAdapter
-        binding.rvQuestions.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+        binding.rvQuestions.addItemDecoration(
+            DividerItemDecoration(
+                this@MainActivity,
+                DividerItemDecoration.VERTICAL
+            )
+        )
 
         questions.add(Question("A 'val' and 'var' are the same.", false))
         questions.add(Question("Mobile Application Developments grants 12 ECTS.", false))
         questions.add(Question("A Unit in Kotlin corresponds to a void in Java", true))
         questions.add(Question("In Kotlin 'when' replaces the 'switch' operation in Java.", false))
         questionAdapter.notifyDataSetChanged()
+        createItemTouchHelper().attachToRecyclerView(rvQuestions)
     }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+        var callback = object :
+            ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or
+                    ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false;
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition;
+                val swipedQuestion: Question = questions[position]
+                if (direction == ItemTouchHelper.RIGHT) {
+                    if (swipedQuestion.isTrue) {
+                        displayCorrectSnackbar();
+                    } else {
+                        displayIncorrectSnackbar();
+                    }
+                } else if (direction == ItemTouchHelper.LEFT) {
+                    if (!swipedQuestion.isTrue) {
+                        displayCorrectSnackbar();
+                    } else {
+                        displayIncorrectSnackbar();
+                    }
+                }
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
+
+    private fun displayCorrectSnackbar() {
+        TODO("Not yet implemented")
+    }
+
+    private fun displayIncorrectSnackbar() {
+        TODO("Not yet implemented")
+    }
+
+
 }
