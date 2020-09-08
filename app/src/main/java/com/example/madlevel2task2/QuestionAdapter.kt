aea8dc76634abb2c61cb803a4d3d0e20723fdcd0
Sophie Bushchak <sophie.bushchak@gmail.com>
2020-09-08 11:34:06 +0200

    Init, make layout, make adapter and data class

---
+++ app/src/main/java/com/example/madlevel2task2/QuestionAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.madlevel2task2
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2task2.databinding.ItemQuestionBinding
+
+class PlaceAdapter(private val questions: ArrayList<Question>) :
+    RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_question, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return questions.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.databind(questions[position])
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val binding = ItemQuestionBinding.bind(itemView)
+
+        fun databind(question: Question) {
+            binding.tvQuestion.text = question.questionText
+        }
+    }
+}
