268534d6809eeebbf49dc6a3d30e3ae16b2835d5
Sophie B. <stb.github@gmail.com>
2020-09-19 14:40:23 +0200

    4.1

--- app/src/main/java/com/example/madlevel3task1/RatingFragment.kt
+++ app/src/main/java/com/example/madlevel3task1/RatingFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_rating.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -23,5 +24,13 @@ class RatingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        showRandomAssessableGame()
+    }
+
+    private fun showRandomAssessableGame() {
+        val randomGame = listOf("Red Dead Redemption 2", "Rocket League",
+                "Shadow of the Tomb Raider").random()
+
+        tvGameName.text = randomGame
     }
 }
