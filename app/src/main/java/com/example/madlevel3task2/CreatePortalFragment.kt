fd170d1d71eae0089fd66a2272d651083b173c5f
Sophie Bushchak <sophie.bushchak@gmail.com>
2020-09-20 14:52:41 +0200

    Fix Adapter, add and observe a portal request

--- app/src/main/java/com/example/madlevel3task2/CreatePortalFragment.kt
+++ app/src/main/java/com/example/madlevel3task2/CreatePortalFragment.kt
@@ -5,10 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_create_portal.*
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
+const val PORTAL_REQUEST_KEY = "req_portal"
+const val BUNDLE_PORTAL_TITLE_KEY = "bundle_portal_title"
+const val BUNDLE_PORTAL_URL_KEY = "bundle_portal_url"
+
 class CreatePortalFragment : Fragment() {
 
     override fun onCreateView(
@@ -21,5 +27,26 @@ class CreatePortalFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        btnAddPortal.setOnClickListener{
+            onAddPortal()
+        }
+    }
+
+    private fun onAddPortal() {
+        val args = Bundle()
+        val portalTitle = etPortalTitle.text.toString()
+        val portalUrl = etPortalUrl.text.toString()
+
+        if (portalTitle.isNotBlank() && portalUrl.isNotBlank()) {
+            setFragmentResult(PORTAL_REQUEST_KEY, bundleOf(Pair(BUNDLE_PORTAL_TITLE_KEY, portalTitle), Pair(
+                BUNDLE_PORTAL_URL_KEY, portalUrl)))
+            findNavController().popBackStack()
+        }  else {
+            Toast.makeText(
+                activity,
+                "Invalid portal", Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
--- app/src/main/java/com/example/madlevel3task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel3task2/MainActivity.kt
@@ -6,18 +6,26 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        navController = findNavController(R.id.nav_host_fragment)
+
+        fab.setOnClickListener {
+            navController.navigate(
+                R.id.action_portalFragment_to_createPortalFragment
+            )
         }
+        fabToggler()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -33,6 +41,16 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun fabToggler() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id in arrayOf(R.id.createPortalFragment)) {
+                fab.hide()
+            } else {
+                fab.show()
+            }
         }
     }
 }
