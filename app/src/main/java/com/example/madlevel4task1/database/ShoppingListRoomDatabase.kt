eed88f6341b6bc7f4958e7a4860fe7f14e875f9e
Sophie B. <stb.github@gmail.com>
2020-09-22 14:19:11 +0200

    Fixed stupid mistake, done

--- app/src/main/java/com/example/madlevel4task1/database/ShoppingListRoomDatabase.kt
+++ app/src/main/java/com/example/madlevel4task1/database/ShoppingListRoomDatabase.kt
@@ -7,35 +7,32 @@ import androidx.room.RoomDatabase
 import com.example.madlevel4task1.dao.ProductDao
 import com.example.madlevel4task1.model.Product
 
-class ShoppingListRoomDatabase {
-    @Database(entities = [Product::class], version = 1, exportSchema = false)
-    abstract class ShoppingListRoomDatabase : RoomDatabase() {
+@Database(entities = [Product::class], version = 1, exportSchema = false)
+abstract class ShoppingListRoomDatabase : RoomDatabase() {
 
-        abstract fun productDao(): ProductDao
+    abstract fun productDao(): ProductDao
 
-        companion object {
-            private const val DATABASE_NAME = "SHOPPING_LIST_DATABASE"
+    companion object {
+        private const val DATABASE_NAME = "SHOPPING_LIST_DATABASE"
 
-            @Volatile
-            private var shoppingListRoomDatabaseInstance: ShoppingListRoomDatabase? = null
+        @Volatile
+        private var shoppingListRoomDatabaseInstance: ShoppingListRoomDatabase? = null
 
-            fun getDatabase(context: Context): ShoppingListRoomDatabase? {
-                if (shoppingListRoomDatabaseInstance == null) {
-                    synchronized(ShoppingListRoomDatabase::class.java) {
-                        if (shoppingListRoomDatabaseInstance == null) {
-                            shoppingListRoomDatabaseInstance =
-                                Room.databaseBuilder(
-                                    context.applicationContext,
-                                    ShoppingListRoomDatabase::class.java,
-                                    DATABASE_NAME
-                                ).build()
-                        }
+        fun getDatabase(context: Context): ShoppingListRoomDatabase? {
+            if (shoppingListRoomDatabaseInstance == null) {
+                synchronized(ShoppingListRoomDatabase::class.java) {
+                    if (shoppingListRoomDatabaseInstance == null) {
+                        shoppingListRoomDatabaseInstance =
+                            Room.databaseBuilder(
+                                context.applicationContext,
+                                ShoppingListRoomDatabase::class.java,
+                                DATABASE_NAME
+                            ).build()
                     }
                 }
-                return shoppingListRoomDatabaseInstance
             }
+            return shoppingListRoomDatabaseInstance
         }
-
     }
 
 }
