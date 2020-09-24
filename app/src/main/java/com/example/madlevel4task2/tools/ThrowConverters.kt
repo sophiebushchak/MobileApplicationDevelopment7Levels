eca0f986c31e0c8ea073354b18c3033906f39eaa
Sophie B. <stb.github@gmail.com>
2020-09-24 14:26:54 +0200

    Fix database and can go to history

--- app/src/main/java/com/example/madlevel4task2/tools/ThrowConverters.kt
+++ app/src/main/java/com/example/madlevel4task2/tools/ThrowConverters.kt
@@ -4,13 +4,5 @@ import androidx.room.TypeConverter
 import com.example.madlevel4task2.model.Throw
 
 class ThrowConverters {
-    @TypeConverter
-    fun fromStringThrowEnum(value: String): Throw? {
-        return enumValueOf<Throw>(value)
-    }
 
-    @TypeConverter
-    fun enumThrowToString(value: Throw?): String? {
-        return value?.toString()
-    }
 }
