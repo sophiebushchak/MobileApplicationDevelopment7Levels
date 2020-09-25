0d9f3269127bdb5b7fdde12689bc3ead34485a51
Sophie Bushchak <sophie.bushchak@gmail.com>
2020-09-25 12:33:42 +0200

    ReminderViewModel

--- app/src/main/java/com/example/madlevel5example/repository/ReminderRepository.kt
+++ app/src/main/java/com/example/madlevel5example/repository/ReminderRepository.kt
@@ -1,6 +1,8 @@
 package com.example.madlevel5example.repository
 
 import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.madlevel5example.database.ReminderRoomDatabase
 import com.example.madlevel5example.dao.ReminderDao
 import com.example.madlevel5example.model.Reminder
@@ -17,8 +19,8 @@ public class ReminderRepository(context: Context) {
         reminderDao = reminderRoomDatabase!!.reminderDao()
     }
 
-    suspend fun getAllReminders(): List<Reminder> {
-        return reminderDao.getAllReminders()
+    fun getAllReminders(): LiveData<List<Reminder>> {
+        return reminderDao?.getAllReminders() ?: MutableLiveData(emptyList())
     }
 
     suspend fun insertReminder(reminder: Reminder) {
