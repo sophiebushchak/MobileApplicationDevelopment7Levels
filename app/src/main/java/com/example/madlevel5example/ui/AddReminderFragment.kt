321ba07bb2a75b623d5129ff40bcd04f35155355
Sophie B. <stb.github@gmail.com>
2020-09-25 12:38:52 +0200

    Update AddRemindersFragment, Done

--- app/src/main/java/com/example/madlevel5example/ui/AddReminderFragment.kt
+++ app/src/main/java/com/example/madlevel5example/ui/AddReminderFragment.kt
@@ -8,17 +8,19 @@ import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.madlevel5example.R
+import com.example.madlevel5example.model.Reminder
 import kotlinx.android.synthetic.main.fragment_add_reminder.*
-
-const val REQ_REMINDER_KEY = "req_reminder"
-const val BUNDLE_REMINDER_KEY = "bundle_reminder"
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class AddReminderFragment : Fragment() {
+    private val viewModel: ReminderViewModel by viewModels()
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,10 +41,7 @@ class AddReminderFragment : Fragment() {
         val reminderText = etReminder.text.toString()
 
         if (reminderText.isNotBlank()) {
-            //set the data as fragmentResult, we are listening for REQ_REMINDER_KEY in RemindersFragment!
-            setFragmentResult(
-                REQ_REMINDER_KEY, bundleOf(Pair(
-                    BUNDLE_REMINDER_KEY, reminderText)))
+            viewModel.insertReminder(Reminder(reminderText))
 
             //"pop" the backstack, this means we destroy
             //this fragment and go back to the RemindersFragment
