5d20dddd06bd56a4921b865c5cc688e643148219
Sophie Bushchak <sophie.bushchak@gmail.com>
2020-09-25 13:40:00 +0200

    Done

--- app/src/main/java/com/example/madlevel5task1/ui/AddNoteFragment.kt
+++ app/src/main/java/com/example/madlevel5task1/ui/AddNoteFragment.kt
@@ -6,13 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.madlevel5task1.R
+import com.example.madlevel5task1.viewmodel.NoteViewModel
+import kotlinx.android.synthetic.main.fragment_add_note.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class AddNoteFragment : Fragment() {
+
+    private val viewModel: NoteViewModel by viewModels()
+
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -24,5 +32,39 @@ class AddNoteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        btnSave.setOnClickListener {
+            saveNote()
+        }
+
+        observeNote()
+    }
+
+    private fun observeNote() {
+        //fill the text fields with the current text and title from the viewmodel
+        viewModel.note.observe(viewLifecycleOwner, Observer {
+            note ->
+            note?.let{
+                tilNoteTitle.editText?.setText(it.title)
+                tilNoteText.editText?.setText(it.text)
+            }
+        })
+
+        viewModel.error.observe(viewLifecycleOwner, Observer {
+            message ->
+            Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+        })
+
+        viewModel.success.observe(viewLifecycleOwner, Observer {
+            succes ->
+            //"pop" the backstack, this means we destroy this fragment and go back to
+            // the RemindersFragment
+            findNavController().popBackStack()
+        })
+    }
+
+    private fun saveNote() {
+        viewModel.updateNote(tilNoteTitle.editText?.text.toString(),
+            tilNoteText.editText?.text.toString())
     }
 }
