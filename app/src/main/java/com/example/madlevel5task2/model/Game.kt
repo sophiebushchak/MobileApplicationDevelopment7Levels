104fb2ca70995dae8c174271ec10e1f7f0d42ab2
Sophie Bushchak <sophie.bushchak@gmail.com>
2020-09-25 14:57:22 +0200

    Game Model, refactor into packages

---
+++ app/src/main/java/com/example/madlevel5task2/model/Game.kt
@@ -0,0 +1,9 @@
+package com.example.madlevel5task2.model
+
+import java.util.*
+
+data class Game(
+    val title: String,
+    val platform: String,
+    val releaseDate: Date
+)
--- app/src/main/java/com/example/madlevel5task2/ui/MainActivity.kt
+++ app/src/main/java/com/example/madlevel5task2/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.madlevel5task2
+package com.example.madlevel5task2.ui
 
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.madlevel5task2.R
 
 class MainActivity : AppCompatActivity() {
 
