42d485e59b2a9cde18732a555d94e0c5cc7781f6
Sophie Bushchak <sophie.bushchak@gmail.com>
2020-09-25 12:51:20 +0200

    Refactor into packages and refactor fragment names

--- app/src/main/java/com/example/madlevel5task1/ui/NotepadFragment.kt
+++ app/src/main/java/com/example/madlevel5task1/ui/NotepadFragment.kt
@@ -1,4 +1,4 @@
-package com.example.madlevel5task1
+package com.example.madlevel5task1.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,18 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.madlevel5task1.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class NotepadFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_add_note, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
