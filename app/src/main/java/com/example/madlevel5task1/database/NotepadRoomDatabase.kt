ca24641b51c2ae7bc5c4cd665f798fa4181c1dc6
Sophie B. <stb.github@gmail.com>
2020-09-25 13:00:51 +0200

    Converters

--- app/src/main/java/com/example/madlevel5task1/database/NotepadRoomDatabase.kt
+++ app/src/main/java/com/example/madlevel5task1/database/NotepadRoomDatabase.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.madlevel5task1.model.Note
 
 @Database(entities = [Note::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class NotepadRoomDatabase : RoomDatabase() {
 
     abstract fun noteDao(): NoteDao
