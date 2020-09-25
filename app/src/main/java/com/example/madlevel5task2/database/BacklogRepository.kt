0dd3f0ac31a5d4ef6345b3e06a6008aae6e4b1b1
Sophie B. <stb.github@gmail.com>
2020-09-25 15:18:10 +0200

    Database setup

---
+++ app/src/main/java/com/example/madlevel5task2/database/BacklogRepository.kt
@@ -0,0 +1,33 @@
+package com.example.madlevel5task2.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.madlevel5task2.model.Game
+
+class BacklogRepository(context: Context) {
+    private val backlogDao: BacklogDao
+
+    init {
+        val database = GameBacklogDatabase.getDatabase(context)
+        backlogDao = database!!.backlogDao()
+    }
+
+    fun getBacklog(): LiveData<Game> {
+        return backlogDao.getBacklog()
+    }
+
+    suspend fun insertGame(game: Game) {
+        return backlogDao.insertGame(game)
+    }
+
+    suspend fun deleteGameFromBacklog(game: Game) {
+        return backlogDao.deleteGameFromBacklog(game)
+    }
+    
+    suspend fun deleteBacklog() {
+        return backlogDao.deleteBacklog()
+    }
+}
