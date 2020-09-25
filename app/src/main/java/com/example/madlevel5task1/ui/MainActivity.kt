84327aa7a79cac8a37e101de0b6efc4cd1ad4864
Sophie B. <stb.github@gmail.com>
2020-09-25 13:40:00 +0200

    Done

--- app/src/main/java/com/example/madlevel5task1/ui/MainActivity.kt
+++ app/src/main/java/com/example/madlevel5task1/ui/MainActivity.kt
@@ -6,14 +6,36 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.madlevel5task1.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        fab.setOnClickListener {
+            navController.navigate(R.id.action_notepadFragment_to_addNoteFragment)
+        }
+
+        fabToggler()
+    }
+
+    private fun fabToggler() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id in arrayOf(R.id.addNoteFragment)) {
+                fab.hide()
+            } else {
+                fab.show()
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
