75ccc2600d39d6cdde24619a119692a6c0acd573
Sophie Bushchak <sophie.bushchak@gmail.com>
2020-09-27 01:41:03 +0200

    Done

--- app/src/main/java/com/example/madlevel6example/ui/TriviaFragment.kt
+++ app/src/main/java/com/example/madlevel6example/ui/TriviaFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.example.madlevel6example.R
+import com.example.madlevel6example.vm.TriviaViewModel
+import kotlinx.android.synthetic.main.fragment_trivia.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class TriviaFragment : Fragment() {
+    private val viewModel: TriviaViewModel by activityViewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +28,18 @@ class TriviaFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        observeTrivia()
     }
+
+    private fun observeTrivia() {
+        viewModel.trivia.observe(viewLifecycleOwner, Observer {
+            tvTrivia.text = it?.text
+        })
+
+        // Observe the error message.
+        viewModel.errorText.observe(viewLifecycleOwner, Observer {
+            Toast.makeText(activity, it, Toast.LENGTH_SHORT).show()
+        })
+    }
+
 }
