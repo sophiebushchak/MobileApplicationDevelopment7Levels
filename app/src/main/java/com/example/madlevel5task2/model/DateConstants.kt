81f6339f9876aff0e2e0abf3b191ba3580ab4335
Sophie B. <stb.github@gmail.com>
2020-09-26 15:56:02 +0200

    Exception handling

---
+++ app/src/main/java/com/example/madlevel5task2/model/DateConstants.kt
@@ -0,0 +1,12 @@
+package com.example.madlevel5task2.model
+
+class DateConstants {
+    companion object {
+        val DAY_MIN = 1
+        val DAY_MAX = 31
+        val MONTH_MIN = 1
+        val MONTH_MAX = 12
+        val YEAR_MIN = 1900
+        val YEAR_MAX = 2100
+    }
+}
