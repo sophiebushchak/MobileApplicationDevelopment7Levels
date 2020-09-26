8d1ee589a094a728d636dec32f4191f12ef9ab8c
Sophie B. <stb.github@gmail.com>
2020-09-27 01:29:12 +0200

    Layout

---
+++ app/src/main/java/com/example/madlevel6example/model/Trivia.kt
@@ -0,0 +1,10 @@
+package com.example.madlevel6example.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Trivia(
+    @SerializedName("text") var text: String,
+    @SerializedName("number") var number: Int,
+    @SerializedName("found") var found: Boolean,
+    @SerializedName("type") var type: String
+)
