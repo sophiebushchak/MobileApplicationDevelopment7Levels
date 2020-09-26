90eb0c52c18faed3d1f3b8fcfdf4acea94c75529
Sophie Bushchak <sophie.bushchak@gmail.com>
2020-09-26 15:56:02 +0200

    Exception handling

--- app/src/main/java/com/example/madlevel5task2/vm/BacklogViewModel.kt
+++ app/src/main/java/com/example/madlevel5task2/vm/BacklogViewModel.kt
@@ -6,14 +6,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.madlevel5task2.database.BacklogRepository
 import com.example.madlevel5task2.model.Game
+import com.example.madlevel5task2.model.DateConstants
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
 class BacklogViewModel(application: Application) : AndroidViewModel(application) {
-    private val mainScope = CoroutineScope(Dispatchers.Main)
     private val ioScope = CoroutineScope(Dispatchers.IO)
     private val backlogRepository = BacklogRepository(application.applicationContext)
     val backlog: LiveData<List<Game>> = backlogRepository.getBacklog()
@@ -63,8 +64,26 @@ class BacklogViewModel(application: Application) : AndroidViewModel(application)
 
     private fun returnDateFromFields(releaseDateDay: String, releaseDateYear: String,
                                    releaseDateMonth: String): Date? {
+        try {
+            if (releaseDateDay.toInt() < DateConstants.DAY_MIN ||
+                releaseDateDay.toInt() > DateConstants.DAY_MAX ||
+                releaseDateMonth.toInt() < DateConstants.MONTH_MIN ||
+                releaseDateMonth.toInt() > DateConstants.MONTH_MAX ||
+                releaseDateYear.toInt() < DateConstants.YEAR_MIN ||
+                releaseDateYear.toInt() > DateConstants.YEAR_MAX
+            ) {
+                return null
+            }
+        } catch (e: java.lang.NumberFormatException) {
+            return null
+        }
         val sdf = SimpleDateFormat("yyyy-MM-dd")
         val dateString = "$releaseDateYear-$releaseDateMonth-$releaseDateDay"
-        return sdf.parse(dateString)
+        try {
+            return sdf.parse(dateString)
+        } catch (e: ParseException) {
+            return null
+        }
+
     }
 }
