2b1a957f374c728998ab4e7d50e3d6e079962788
Sophie Bushchak <sophie.bushchak@gmail.com>
2020-09-27 01:29:12 +0200

    Layout

--- app/src/main/java/com/example/madlevel6example/ui/TriviaFragment.kt
+++ app/src/main/java/com/example/madlevel6example/ui/TriviaFragment.kt
@@ -1,10 +1,11 @@
-package com.example.madlevel6example
+package com.example.madlevel6example.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.madlevel6example.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
