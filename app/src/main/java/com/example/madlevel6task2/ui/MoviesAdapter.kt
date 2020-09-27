063b56689ed6bb29b0feab144afeef4611dec51d
Sophie Bushchak <sophie.bushchak@gmail.com>
2020-09-27 19:18:27 +0200

    Fix UI breaking and implement adapter

---
+++ app/src/main/java/com/example/madlevel6task2/ui/MoviesAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.madlevel6task2.ui
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.madlevel6task2.R
+import com.example.madlevel6task2.model.MovieResult
+import kotlinx.android.synthetic.main.item_movie.view.*
+
+class MoviesAdapter(private val movies: List<MovieResult>, private val onClick: (MovieResult) -> Unit) :
+    RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
+
+    private lateinit var context: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+
+        return ViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(movies[position])
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        init {
+            itemView.setOnClickListener { onClick(movies[adapterPosition]) }
+        }
+
+        fun bind(movie: MovieResult) {
+            Glide.with(context).load("http://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg").into(itemView.ivImagePoster)
+            itemView.tvMovieTitle.text = movie.title
+            itemView.tvPosition.text = (movies.indexOf(movie) + 1).toString() + "."
+        }
+    }
+
+}
