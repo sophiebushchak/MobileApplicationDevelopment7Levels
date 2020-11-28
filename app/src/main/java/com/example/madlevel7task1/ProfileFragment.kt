11319281f99ff92ed8f91e6585c2389d9339382d
Sophie B. <stb.github@gmail.com>
2020-11-28 13:59:14 +0100

    Done

--- app/src/main/java/com/example/madlevel7task1/ProfileFragment.kt
+++ app/src/main/java/com/example/madlevel7task1/ProfileFragment.kt
@@ -1,17 +1,22 @@
 package com.example.madlevel7task1
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class ProfileFragment : Fragment() {
+    private val viewModel: ProfileViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +28,21 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        observeProfile()
+    }
+
+    private fun observeProfile() {
+        viewModel.getProfile()
+
+        viewModel.profile.observe(viewLifecycleOwner, Observer {
+            val profile = it
+
+            tvName.text = getString(R.string.profile_name, profile.firstName, profile.lastName)
+            tvDescription.text = profile.description
+            if (profile.imageUri!!.isNotEmpty()) {
+                ivProfileImage.setImageURI(Uri.parse(profile.imageUri))
+            }
+        })
     }
 }
