70ab6651e1f0ff8634f10d3916fc1c9716979698
Sophie Bushchak <sophie.bushchak@gmail.com>
2020-11-28 13:34:40 +0100

    Step 2 done

---
+++ app/src/main/java/com/example/madlevel7task1/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.madlevel7task1
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            FirebaseFirestore.setLoggingEnabled(true)
+            FirebaseApp.initializeApp(this)
+            findNavController(R.id.nav_host_fragment_container).navigate(R.id.createProfileFragment)
+        }
+    }
+}
