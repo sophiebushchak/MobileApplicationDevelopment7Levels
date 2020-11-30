9f61af106fdb4b2d6b1e4f86a853cb5a18748f68
Sophie Bushchak <sophie.bushchak@gmail.com>
2020-11-30 13:15:33 +0100

    Quiz Session playing functionality

--- app/src/main/java/com/example/madlevel7task2/ui/QuizFragment.kt
+++ app/src/main/java/com/example/madlevel7task2/ui/QuizFragment.kt
@@ -1,10 +1,13 @@
 package com.example.madlevel7task2.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.Observer
@@ -33,12 +36,40 @@ class QuizFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         observeQuizSession()
         observeReceivedQuiz()
+        initializeViews()
     }
 
-    private fun setViews(quizSession: QuizSession) {
+    private fun initializeViews() {
+        btnConfirmAnswer.setOnClickListener {
+            confirmAnswer()
+        }
+        answerOptionsRadioGroup.check(0)
+    }
+
+    private fun confirmAnswer() {
+        val rbChosenId = answerOptionsRadioGroup.checkedRadioButtonId
+        val rb: RadioButton = answerOptionsRadioGroup.findViewById(rbChosenId)
+        quizSessionViewModel.answerQuestion(rb.text.toString())
+    }
+
+    private fun updateViews(quizSession: QuizSession) {
         tvQuizIndicator.text = quizSession.getQuizTitle()
         tvQuestionIndicator.text = "${quizSession.getCurrentQuestionNumber()}/${quizSession.getTotalQuestionNumber()}"
         tvQuestionText.text = quizSession.getCurrentQuestion().quizQuestionText
+        fillRadioGroup(quizSession);
+    }
+
+    private fun fillRadioGroup(quizSession: QuizSession) {
+        val radioGroup = answerOptionsRadioGroup
+        radioGroup.removeAllViews()
+        var radioButtonIncrements = 0
+        for (quizAnswer in quizSession.getCurrentQuestion().quizAnswers) {
+            val rb = RadioButton(requireContext());
+            rb.text = quizAnswer.answerText
+            rb.id = radioButtonIncrements
+            radioGroup.addView(rb)
+            radioButtonIncrements += 1
+        }
     }
 
     private fun observeReceivedQuiz() {
@@ -51,7 +82,7 @@ class QuizFragment : Fragment() {
 
     private fun observeQuizSession() {
         quizSessionViewModel.quizSession.observe(viewLifecycleOwner, Observer {
-            setViews(
+            updateViews(
                 it
             )
         })
--- app/src/main/java/com/example/madlevel7task2/vm/QuizSessionViewModel.kt
+++ app/src/main/java/com/example/madlevel7task2/vm/QuizSessionViewModel.kt
@@ -5,13 +5,34 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.madlevel7task2.model.Quiz
+import com.example.madlevel7task2.model.QuizAnswer
 import com.example.madlevel7task2.model.QuizSession
 
 class QuizSessionViewModel(application: Application) : AndroidViewModel(application) {
     private val _quizSession: MutableLiveData<QuizSession> = MutableLiveData()
     val quizSession: LiveData<QuizSession> get() = _quizSession
+    lateinit var currentSession: QuizSession;
 
     fun startQuizSession(quiz: Quiz) {
-        _quizSession.value = QuizSession(quiz)
+        currentSession = QuizSession(quiz)
+        broadcastSessionChanged()
+    }
+
+    fun answerQuestion(answerText: String) {
+        var quizAnswer: QuizAnswer? = null
+        for (q in currentSession.getCurrentQuestion().quizAnswers) {
+            if (q.answerText == answerText) {
+                quizAnswer = QuizAnswer(answerText)
+            }
+        }
+        if (quizAnswer != null) {
+            currentSession.answerQuestion(quizAnswer, currentSession.getCurrentQuestion())
+        }
+        currentSession.advanceCurrentQuestion()
+        broadcastSessionChanged()
+    }
+
+    private fun broadcastSessionChanged() {
+        _quizSession.value = currentSession
     }
 }
