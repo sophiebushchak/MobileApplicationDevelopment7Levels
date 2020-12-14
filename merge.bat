d2fdad86237fa4dc29e652ab0b254f27f0ddbe6f
Sophie Bushchak <sophie.bushchak@gmail.com>
2020-12-14 13:39:42 +0100

    Folder structure and README

--- merge.bat
+++
@@ -1,5 +0,0 @@
-set /p repository="Enter Repository "
-git remote add -f %repository% https://github.com/sophiebushchak/%repository%
-git merge %repository%/master --allow-unrelated-histories
-mkdir %repository%
-pause
