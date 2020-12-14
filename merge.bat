7fad2c58b8717dd685a3e4e342c1d7c2bb3afa29
Sophie B. <stb.github@gmail.com>
2020-12-14 12:34:15 +0100

    Merge MadLevel2Example

---
+++ merge.bat
@@ -0,0 +1,4 @@
+set /p repository="Enter Repository "
+git remote add -f %repository% https://github.com/sophie-thea-b/%repository%
+git merge %repository%/master --allow-unrelated-histories
+mkdir %repository%
