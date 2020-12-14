7eb05dd56868998bb42851b27ddbf6815973ce1c
Sophie Bushchak <sophie.bushchak@gmail.com>
2020-12-14 12:34:15 +0100

    Merge MadLevel2Example

---
+++ merge.bat
@@ -0,0 +1,4 @@
+set /p repository="Enter Repository "
+git remote add -f %repository% https://github.com/sophiebushchak/%repository%
+git merge %repository%/master --allow-unrelated-histories
+mkdir %repository%
